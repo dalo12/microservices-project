d6b87658f6501f5fc47cb2c1f4c8d1d699cc6985
David López <dalo_vidpez@hotmail.com.ar>
2025-11-26 18:34:02 -0300

    Movies: changed connection string

--- movies/config/database.js
+++ movies/config/database.js
@@ -4,11 +4,8 @@ const connectDB = async () => {
   try {
     // Assuming MongoDB is running on default port 27017
     // If using Docker, it might be localhost:27017
-    const conn = await mongoose.connect('mongodb://localhost:27017/sample_mflix', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect('mongodb://admin:12345@localhost:27017/sample_mflix?authSource=admin');
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error);
